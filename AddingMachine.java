09b880158dc31724499ba65cdce754c7fe272fd8
jcahal <jacahal@gmail.com>
2019-10-09 11:29:47 -0700

    initial commit

---
+++ AddingMachine.java
@@ -0,0 +1,80 @@
+package cse360assign2;
+
+/**
+ * This Class implements a simple arithmetic machine
+ * 
+ * 
+ * @author JonathanCahal<br>
+ * CSE 360<br>
+ * Assignment 2
+ * 
+ * @since 1.0
+ * @version 1.0
+ * 
+ * @param total The total after calculation
+ *
+ */
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Default  for the AddingMachine Class.
+	 * 
+	 */
+	public AddingMachine () {
+		
+		total = 0;  // not needed - included for clarity
+	
+	}
+	
+	/**
+	 * getTotal function for the result of calculation.
+	 * 
+	 * @return the result of the calculation.
+	 */
+	public int getTotal () {
+		
+		return 0;
+	
+	}
+	
+	/**
+	 * Basic add function for the AddingMachine Class.
+	 * 
+	 * @param value the int value to be added.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Basic subtract function for the AddingMachine Class.
+	 * 
+	 * @param value the int value to be subtracted.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Turns the value of the AddingMachine to a String.
+	 * 
+	 * @return The String equivalent of an AddingMachine instance
+	 */
+	public String toString () {
+		
+		return "";
+		
+	}
+	
+	/**
+	 * Clears the history of an AddingMachine instance
+	 * 
+	 */
+	public void clear() {
+		
+	}
+	
+}
